30361c713586887b9a407cce9264e90a509ac1a1
Lescourret <90576270+Lescourret@users.noreply.github.com>
2021-11-14 13:16:27 +0100

    Update fusionner_listes.ml

--- outils_liste/fusionner_listes.ml
+++ outils_liste/fusionner_listes.ml
@@ -4,5 +4,5 @@
 let rec fusion l1 l2 = match l1, l2 with
 | [], _ -> l2 (*cas de base*)
 | _, [] -> l1 (*cas de base*)
-| e1::q1, e2::q2 when e1 < e2 -> e1::fusion q1 l2 (*tous les éléments de la liste l1 fusionné dans la liste finale*)
-| e1::q1, e2::q2 -> e2::fusion l1 q2 (*puis, tous les éléments de l2*)
+| e1::q1, e2::q2 when e1 < e2 -> e1::fusion q1 l2 (* Si l'élément e1 est plus petit que e2 alors il est fusionné à la liste finale*)
+| e1::q1, e2::q2 -> e2::fusion l1 q2 (*sinon, c'est l'élément e2 *)
